f4d235d5284119817092ee747b5b0e3d5ebc26e6
cj <cjlazell@gmail.com>
2013-05-14 17:05:54 -0500

    moved permissions into a folder

--- lib/active_record/acts/indulgent.rb
+++ lib/active_record/acts/indulgent.rb
@@ -4,27 +4,27 @@ module ActiveRecord
       def self.included(base)
         base.send :extend, ClassMethods
       end
-      
+
       module ClassMethods
-        
+
         def acts_as_indulgent(args = {})
           include Indulgence::Indulgent::InstanceMethods
           extend Indulgence::Indulgent::ClassMethods
-          
+
           alias_method args[:compare_single_method], :indulge? if args[:compare_single_method]
           singleton_class.send(:alias_method, args[:filter_many_method], :indulgence) if args[:filter_many_method]
           self.indulgent_permission_class = args[:using]
         end
-        
+
         def indulgent_permission_class
           @indulgent_permission_class
         end
-        
+
         private
         def default_indulgence_permission_class
           class_name = to_s
           name = class_name + "Permission"
-          require name.underscore
+          require "permissions/#{name.underscore}"
           if const_defined? name
             class_eval name
           end
@@ -33,7 +33,7 @@ module ActiveRecord
         def indulgent_permission_class=(klass = nil)
           @indulgent_permission_class = klass || default_indulgence_permission_class
         end
-        
+
       end
     end
   end
