2d4b1e81849ed4a0bdc3d790824986217b4792ea
cj <cjlazell@gmail.com>
2013-05-21 17:55:01 -0500

    update

--- lib/indulgence/ability.rb
+++ lib/indulgence/ability.rb
@@ -2,7 +2,7 @@
 module Indulgence
   class Ability
     attr_reader :name, :entity_id_method, :args
-    
+
     def initialize(args = {})
       @name = args[:name]
       @compare_single = args[:compare_single]
@@ -11,14 +11,14 @@ module Indulgence
       @args = args
       valid?
     end
-    
+
     def ==(another_ability)
       [:name, :args].each do |method|
         return false if send(method) != another_ability.send(method)
       end
       return true
     end
-    
+
     def valid?
       must_be_name
       unless entity_id_method
@@ -26,36 +26,36 @@ module Indulgence
         must_respond_to_call @filter_many
       end
     end
-    
+
     def compare_single(thing, entity)
       if entity_id_method
         thing.send(entity_id_method) == entity.id
       else
-        @compare_single.call thing, entity   
+        @compare_single.call thing, entity
       end
     end
-    
+
     def filter_many(things, entity)
       if entity_id_method
         things.where(entity_id_method => entity.id)
       else
-        @filter_many.call things, entity   
+        @filter_many.call things, entity
       end
     end
-    
+
     private
     def must_be_name
       unless name
         raise AbilityConfigurationError, "A name is required for each ability"
       end
     end
-    
+
     def must_respond_to_call(item)
       unless item.respond_to? :call
         raise AbilityConfigurationError, "item must respond to call: #{item.inspect}"
       end
     end
-    
-    
+
+
   end
 end
